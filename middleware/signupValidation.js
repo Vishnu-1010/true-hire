5351b1aa3d9cc5fbdb12ce7b93b2fd3f57b7a0a2
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-02-21 21:03:44 +0530

    spacing to signup validation

--- middleware/signupValidation.js
+++ middleware/signupValidation.js
@@ -1,17 +1,16 @@
-const validateSignUp = (req,res,next)=>{
-    const {fullName,email,password,role} = req.body;
-    if(!fullName.trim() || !email.trim() || !password.trim() || !role.trim()){
-        return res.json({
-            status:400,
-            message:"All fields are require"
-        });
-
-    }
-    if(password.length()<6){
-        return res.json({
-            status:400,
-            message:"password length must be greater than 8"
-        });
-    }
-    next();
-}
+const validateSignUp = (req, res, next) => {
+  const { fullName, email, password, role } = req.body;
+  if (!fullName.trim() || !email.trim() || !password.trim() || !role.trim()) {
+    return res.json({
+      status: 400,
+      message: "All fields are require",
+    });
+  }
+  if (password.length() < 6) {
+    return res.json({
+      success: false,
+      message: "password length must be greater than 8",
+    });
+  }
+  next();
+};
