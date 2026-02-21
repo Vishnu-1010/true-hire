a6e8eb2ff83f91f0975631809958b56302b9d6e8
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-02-21 19:51:25 +0530

    updated signup schema

---
+++ backend/models/signup.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["candidate", "employer", "admin"],
+      default: "Candidate",
+    },
+    isActive: { type: Boolean, default: true },
+    isVerified: { type: Boolean, default: false },
+    },
+  { timeseries: true },
+);
+userSchema.pre("save", async function (next) {
+  // encrypting password before saving to database
+  if (!this.isModified("password")) {
+    return next();
+  }else{
+    const salt = bcrypt.genSaltSync((process.env.SALT_ROUNDS )|| 12);
+    const hash = bcrypt.hashSync(this.password, salt);
+    this.password = hash;
+    next();
+  }
+});
+
+// password comparison method
+userSchema.methods.passwordCompare = async function(pass){
+  return await bcrypt.compare(pass,this.password);
+}
+export default mongoose.model("userRegister", userSchema);
