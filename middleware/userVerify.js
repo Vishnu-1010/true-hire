8129f14ea2f645f531a67634ac4906d28ad902e9
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-02-22 20:45:22 +0530

    Modifyed user Verfy using cookies

--- middleware/userVerify.js
+++ middleware/userVerify.js
@@ -1,18 +1,18 @@
-import express from "express";
-const app = express();
-import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
-app.use(cookieParser());
+
 const userVerify = (req, res, next) => {
   const verifyToken = req.cookies.Token;
 
-  if (!verifyToken) return res.status(400).json({ message: "Invalid token" });
-  const ans = jwt.verify(verifyToken, process.env.SECRET, (err, data) => {
-    if (err) return res.status(400).json({ message: "Invaild User" });
-    res.send("user sucess");
+  if (!verifyToken) {
+    return res.status(400).json({ message: "Invalid token" });
+  }
+
+  jwt.verify(verifyToken, process.env.JWT_SECRET, (err, data) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid User" });
+    }
     req.user = data;
     next();
   });
-  console.log(ans);
 };
 export { userVerify };
