07e1fcd5909fa3815c6f752395ecd083c2cdacd6
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-02-22 20:42:42 +0530

    Added signup validation middle ware

--- middleware/signupValidation.js
+++ middleware/signupValidation.js
@@ -1,16 +1,36 @@
 const validateSignUp = (req, res, next) => {
-  const { fullName, email, password, role } = req.body;
-  if (!fullName.trim() || !email.trim() || !password.trim() || !role.trim()) {
-    return res.json({
-      status: 400,
-      message: "All fields are require",
+  let { fullName, email, password, role } = req.body;
+  req.body.email = email.toLowerCase();
+  if (
+    !fullName?.trim() ||
+    !email?.trim() ||
+    !password?.trim() ||
+    !role?.trim()
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: "All fields are required",
     });
   }
-  if (password.length() < 6) {
-    return res.json({
+  if (password.length < 8) {
+    return res.status(400).json({
       success: false,
       message: "password length must be greater than 8",
     });
   }
+  const emailRegression = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegression.test(email)) {
+    return res.status(400).json({
+      success: false,
+      message: "enter a valid email address",
+    });
+  }
+  const validRoles = ["candidate", "recruiter"];
+  if (!validRoles.includes(role.toLowerCase())) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid role selected" });
+  }
   next();
 };
+export { validateSignUp };
