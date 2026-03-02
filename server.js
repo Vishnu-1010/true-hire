21a6c8d13da5a5a36b71cfbe4e12892048cf3ff9
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-03-02 22:38:00 +0530

    setting up cors

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
-import bcrypt from "bcrypt";
-
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/mongodb.js";
 import { homePage, userLogin, userSignup } from "./controllers/userAuth.js";
-
+import cors from "cors";
 const app = express();
 const port = process.env.PORT;
 
@@ -17,6 +15,14 @@ connectDB()
   .catch(() => {
     console.log("unable to connect to db");
   });
+//setting up cors
+app.use(
+  cors({
+    origin: ["http://localhost/5173", "http://localhost/3000"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,7 +31,6 @@ app.use(cookieParser());
 
 // to parse all middle ware to routes
 app.use(userRoutes);
-
 
 app.use("/login", userLogin);
 app.use("/signup", userSignup);
