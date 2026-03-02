e6d298ef80a1c4f665bd5a334ae113559e6673e0
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-03-02 23:05:32 +0530

    adding candidate profile schema

---
+++ models/candiateProfile.js
@@ -0,0 +1,46 @@
+import mongoose, { Schema } from "mongoose";
+
+const candidateProfile = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "userRegister", // Link to User model
+      required: true,
+    },
+    aboutMe: {
+      type: String,
+      trim: true,
+    },
+    education: {
+      collegeName: { type: String, trim: true },
+      startYear: {
+        type: Number,
+      },
+      endYear: {
+        type: Number,
+      },
+      branch: { type: String, trim: true },
+      cgpa: { type: Number },
+    },
+    skills: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+    experience: [
+      {
+        companyName: String,
+        position: String,
+        startDate: Date,
+        EndDate: Date,
+        description: String,
+      },
+    ],
+    profileImage: String,
+    resume: String,
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("candidateProfile", candidateProfile);
