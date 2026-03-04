6fe14903f3d73f2729ec7f8ee6c6e66c07a9650c
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-03-05 04:00:36 +0530

    Fixed recruiter schema

--- models/recruiterProfile.js
+++ models/recruiterProfile.js
@@ -1,25 +1,30 @@
-import mongoose from 'mongoose';
-const recruiterProfile = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "userRegister",
-    required:true
+import mongoose from "mongoose";
+const recruiterProfile = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "userRegister",
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    companyWebsite: {
+      type: String,
+      trim: true,
+    },
+    companyDescription: {
+      type: String,
+      trim: true,
+    },
+    location: {
+      type: String,
+      trim: true,
+    },
+    recruiterProfileImage: String,
+    companyLogo: String
   },
-  companyName:{
-    type:String,
-    required:true
-  },
-  companyWebsite:{
-    type:String,
-    trim:true
-  },
-  companyDescription:{
-    type:String,
-    trim:true
-  },
-  location:{
-    type:String,
-    trim:true
-  }
-},{timestamps:true});
-module.exports = mongoose.model("recruiterProfile",recruiterProfile);
+  { timestamps: true },
+);
+module.exports = mongoose.model("recruiterProfile", recruiterProfile);
