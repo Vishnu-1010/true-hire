2bb79fbe3410d317afaebbf4013c345d367097a5
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-03-05 03:57:25 +0530

    Added extra validation to verify user through headers

--- middleware/userVerify.js
+++ middleware/userVerify.js
@@ -1,15 +1,16 @@
 import jwt from "jsonwebtoken";
 
 const userVerify = (req, res, next) => {
-  const verifyToken = req.cookies.Token;
+  const verifyToken = req.cookies.Token || req.headers.authorization?.split(" ")[1];
 
+ 
   if (!verifyToken) {
-    return res.status(400).json({ message: "Invalid token" });
+    return res.status(401).json({ success: false, message: "unauthorized" });
   }
 
   jwt.verify(verifyToken, process.env.JWT_SECRET, (err, data) => {
     if (err) {
-      return res.status(403).json({ message: "Invalid User" });
+      return res.status(403).json({ success:false,message: "Invalid User" });
     }
     req.user = data;
     next();
