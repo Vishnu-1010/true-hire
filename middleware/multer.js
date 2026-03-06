d9471c25a4c402b5dc20474a092327819ebd7fa6
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-03-06 21:24:26 +0530

    modified few fields

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,18 +1,30 @@
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadPath = "./uploads/temp";
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination:  function (req, file, cb) {
-    cb(null, "../uploads/temp");
+  destination: function (req, file, cb) {
+    cb(null, uploadPath);
   },
+
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+
+    const ext = path.extname(file.originalname);
+
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
   },
 });
 
 export const upload = multer({
   storage,
   limits: {
-    fieldSize: 5 * 1024 * 1024, // 5mb max limit of the file
+    fileSize: 5 * 1024 * 1024,// 5mb max file size
   },
 });
