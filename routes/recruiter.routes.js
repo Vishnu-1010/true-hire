c2f65f49e544c94466418ec93f7ef62268142537
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-03-09 21:45:13 +0530

    added recruiter routes

---
+++ routes/recruiter.routes.js
@@ -0,0 +1,23 @@
+import express from "express";
+const router = express();
+
+import { userVerify } from "../middleware/userVerify.js";
+import { createRecruiter,getRecruiter,updateRecruiter } from "../controllers/recruiter.controller.js";
+import { recruiterOnly } from "../middleware/recruiterMiddleware.js";
+import { upload } from "../middleware/multer.js";
+router.post(
+  "/profile",
+  userVerify,
+  recruiterOnly,
+  upload.fields([
+    { name: "recruiterImage", maxCount: 1 },
+    { name: "companyLogos", maxCount: 1 },
+  ]),
+  createRecruiter,
+);
+router.get("/profile",userVerify,recruiterOnly,getRecruiter);
+router.put("/profile",userVerify,recruiterOnly, upload.fields([
+    { name: "recruiterImage", maxCount: 1 },
+    { name: "companyLogos", maxCount: 1 },
+  ]),updateRecruiter);
+export default router;
