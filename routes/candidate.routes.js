922a4627f8af3df6a6847b03cc4af23556e8ca31
Vishnu-1010 <chintuvishnureddy10@gmail.com>
2026-03-09 21:44:55 +0530

    added candidate routes

---
+++ routes/candidate.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { createCandidate, getCandidate,updateCandidate} from "../controllers/candidate.controller.js";
+import { userVerify } from "../middleware/userVerify.js";
+import { candidateOnly } from "../middleware/candidateMiddleware.js";
+import { upload } from "../middleware/multer.js";
+
+const router = express.Router();
+
+router.post(
+  "/candidate/profile",
+  userVerify,
+  candidateOnly,
+  upload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "resume", maxCount: 1 },
+  ]),
+  createCandidate,
+);
+router.get("/profile",userVerify,candidateOnly,getCandidate);
+router.put("/profile",userVerify,candidateOnly, upload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "resume", maxCount: 1 },
+  ]),updateCandidate);
+
+export default router;
